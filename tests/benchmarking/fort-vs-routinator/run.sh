96e5206e04d83ad949e171c6676a03f2cf9151dd
Raul Ramirez <raul.ramirez@esceptica.mx>
2026-04-12 23:06:41 -0600

    Benchmarking tests adjustments

---
+++ tests/benchmarking/fort-vs-routinator/run.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+. tools/benchmk.sh
+
+RPKI_DIR="/tmp/rpki"
+CACHE_ROUTINATOR="$RPKI_DIR/cache-routinator"
+CACHE_FORT="$RPKI_DIR/cache-fort"
+RESULTS_DIR="$SANDBOX/results"
+
+mkdir -p "$CACHE_ROUTINATOR"
+mkdir -p "$CACHE_FORT"
+mkdir -p "$RESULTS_DIR"
+
+rm -rf "$CACHE_ROUTINATOR"/*
+rm -rf "$CACHE_FORT"/*
+rm -f "$RESULTS_DIR"/*
+
+# Validators running in parallel
+run_fort_validation_for_rir_tals "$CACHE_FORT" "$RESULTS_DIR/fort-vrps.txt" "$RESULTS_DIR/fort-aspa.txt" &
+PID_FORT=$!
+
+run_routinator_validation_for_rir_tals "$CACHE_ROUTINATOR" "$RESULTS_DIR/routinator-mixed.txt" &
+PID_ROUTINATOR=$!
+
+wait $PID_FORT $PID_ROUTINATOR
+
+# Normilizing outputs for comparition
+normalize_routinator_data \
+    "$RESULTS_DIR/routinator-mixed.txt" \
+    "$RESULTS_DIR/routinator-vrps.sorted" \
+    "$RESULTS_DIR/routinator-aspa.sorted"
+
+normalize_fort_vrps_file \
+    "$RESULTS_DIR/fort-vrps.txt" \
+    "$RESULTS_DIR/fort-vrps.sorted"
+
+normalize_fort_aspa_file \
+    "$RESULTS_DIR/fort-aspa.txt" \
+    "$RESULTS_DIR/fort-aspa.sorted"
+
+# Generating parity report
+check_parity \
+    "$RESULTS_DIR/fort-vrps.sorted" "VPRs FORT" \
+    "$RESULTS_DIR/routinator-vrps.sorted" "VPRs Routinator"
+check_parity \
+    "$RESULTS_DIR/fort-aspa.sorted" "ASPAs FORT" \
+    "$RESULTS_DIR/routinator-aspa.sorted" "ASPAs Routinator"
---
+++ tools/benchmk.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+# Run the validation of the rpki repositories RIRs. 
+# $1: Path where the local repository will be stored
+# $2: Path of the output file with VRPs
+# $3: Path of the output file with ASPAs
+run_fort_validation_for_rir_tals() {
+	TAL_DIR="$SANDBOX/tals"
+	mkdir -p "$TAL_DIR"
+
+	fort --init-tals --tal "$SANDBOX/tals" > /dev/null 2>&1
+
+	start_time=$(date +%s)
+	fort \
+		--mode "standalone" \
+		--tal "$SANDBOX/tals" \
+		--local-repository "$1" \
+		--output.roa "$2" \
+		--output.aspa "$3" 2>/dev/null
+	end_time=$(date +%s)
+	printf "FORT execution time:       %s seconds\n" "$((end_time - start_time))"
+}
+
+# Run the validation of the rpki repositories RIRs. 
+# $1: Path where the local repository will be stored
+# $2: Path of the output file (including VRPs and ASPAs)
+run_routinator_validation_for_rir_tals() {
+	start_time=$(date +%s)
+	routinator \
+		--enable-aspa \
+		--fresh \
+		--repository-dir "$1" \
+		vrps --format json > "$2" 2>/dev/null
+	end_time=$(date +%s)
+	printf "Routinator execution time:       %s seconds\n" "$((end_time - start_time))"
+}
+
+# Divides the Routinator output file into VPRs file and ASPA file, 
+# normalizes them, and sorts them so they can be compared.
+# $1: Original Routinator file
+# $2: VPRs file (normalized and sorted)
+# $3: ASPA file (normalized and sorted)
+normalize_routinator_data() {
+    input_file="$1"
+    vrps_output="$2"
+    aspa_output="$3"
+
+    grep '^[[:space:]]*{ "asn":' "$input_file" | \
+    sed 's/[[:space:]]//g; s/{"asn":"//; s/","prefix":"/,/; s/","maxLength":/,/; s/,"ta":.*//' | \
+    sort -V > "$vrps_output"
+
+	grep '{.*"customer":' $input_file | \
+	sed 's/[[:space:]]//g; s/.*"customer":"AS//; s/","providers":\["AS/ /; s/","AS/ /g; s/"\],"ta":.*//' | \
+	awk '{ printf "\"%s\": [ ", $1; for(i=2; i<=NF; i++) printf "%s%s", $i, (i==NF ? "" : ", "); print " ]" }' | \
+	sort -V > "$aspa_output"
+}
+
+# Normalize a VPRS output file from FORT in order to be compared.
+# $1: Original VRPs file
+# $2: Normalized file
+normalize_fort_vrps_file() {
+    input="$1"
+    output="$2"
+
+    # Fort: Skip header and sort
+    tail -n +2 "$input" | sort -V > "$output"
+}
+
+# Normalize a ASPA output file from Fort in order to be compared.
+# $1: Original VRPs file
+# $2: Normalized file
+normalize_fort_aspa_file() {
+    input="$1"
+    output="$2"
+
+    # Explanation of the sed command:
+    # 1d -> Removes the first line.
+    # $d -> Removes the last line ($ represents the end).
+    # s/^[[:space:]]*// -> Removes leading spaces or tabs (^) from each line.
+    # s/,$// -> Removes the trailing comma (,) ($) from the line.
+    sed -e '1d' -e '$d' -e 's/^[[:space:]]*//' -e 's/,$//' "$input" | sort -V > "$output"
+}
+
+check_parity () {
+	file_name=$(printf "%s%s" "$2" "$4" | tr -d ' ' | tr '[:upper:]' '[:lower:]')
+	DIFF_FILE="$SANDBOX/results/$file_name.diff"
+
+	count_elements_file_rp1=$(wc -l < "$1")
+	count_elements_file_rp2=$(wc -l < "$3")
+	echo "$2: $count_elements_file_rp1"
+	echo "$4: $count_elements_file_rp2"
+
+	if diff -q "$1" "$3" > /dev/null; then
+    	echo "RESULT: Total parity confirmed (100% identical)."
+	else
+    	diff -u "$1" "$3" > "$DIFF_FILE"
+    	echo "RESULT: WARNING: Discrepancies were detected."
+    	echo "See: $DIFF_FILE"
+	fi
+}
