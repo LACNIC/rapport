c24b3fd801783507fe3caf526386847c9bdb0f5f
Alberto Leiva Popper <ydahhrk@gmail.com>
2026-05-29 07:57:52 -0600

    Add more checks to session-depends-on-version test

--- tests/8210bis/33-session-depends-on-version/run.sh
+++ tests/8210bis/33-session-depends-on-version/run.sh
@@ -20,36 +20,41 @@ request_session() {
 	send_router_pdu "reset-query version $1"
 
 	wait_pdus 4
+	# Extract session from the output
 	grep -F "cache-response" "$SANDBOX/barry-rtr.stdout" |
-		awk '{ print $5 }' > "$PDU_DIR/v$1"
-	truncate -s 0 "$SANDBOX/barry-rtr.stdout"
+		awk '{ print $5 }' > "$PDU_DIR/$2-v$1"
 
 	stop_router
+	rm "$SANDBOX/barry-rtr.stdout"
 }
 
 run_barry rd
 start_rp
-request_session 0
-request_session 1
-request_session 2
+request_session 0 "router1"
+request_session 1 "router1"
+request_session 2 "router1"
+request_session 0 "router2"
+request_session 1 "router2"
+request_session 2 "router2"
 stop_rp
 
-SESSION_V0=$(cat "$PDU_DIR/v0")
-SESSION_V1=$(cat "$PDU_DIR/v1")
-SESSION_V2=$(cat "$PDU_DIR/v2")
+SESSION_R1V0=$(cat "$PDU_DIR/router1-v0")
+ck_inc && test ! -z "$SESSION_R1V0" || fail "Missing router1-v0 session. See $PDU_DIR"
+SESSION_R1V1=$(cat "$PDU_DIR/router1-v1")
+ck_inc && test ! -z "$SESSION_R1V1" || fail "Missing router1-v1 session. See $PDU_DIR"
+SESSION_R1V2=$(cat "$PDU_DIR/router1-v2")
+ck_inc && test ! -z "$SESSION_R1V2" || fail "Missing router1-v2 session. See $PDU_DIR"
+SESSION_R2V0=$(cat "$PDU_DIR/router2-v0")
+ck_inc && test ! -z "$SESSION_R2V0" || fail "Missing router2-v0 session. See $PDU_DIR"
+SESSION_R2V1=$(cat "$PDU_DIR/router2-v1")
+ck_inc && test ! -z "$SESSION_R2V1" || fail "Missing router2-v1 session. See $PDU_DIR"
+SESSION_R2V2=$(cat "$PDU_DIR/router2-v2")
+ck_inc && test ! -z "$SESSION_R2V2" || fail "Missing router2-v2 session. See $PDU_DIR"
 
-ck_inc
-test ! -z "$SESSION_V0" || fail "RTRv0 session was not recorded"
-ck_inc
-test ! -z "$SESSION_V1" || fail "RTRv1 session was not recorded"
-ck_inc
-test ! -z "$SESSION_V2" || fail "RTRv2 session was not recorded"
-ck_inc
-test "$SESSION_V0" -ne "$SESSION_V1" ||
-	fail "RTRv0 session equals RTRv1 session. See $PDU_DIR"
-ck_inc
-test "$SESSION_V1" -ne "$SESSION_V2" ||
-	fail "RTRv1 session equals RTRv2 session. See $PDU_DIR"
-ck_inc
-test "$SESSION_V0" -ne "$SESSION_V2" ||
-	fail "RTRv0 session equals RTRv2 session. See $PDU_DIR"
+ck_inc && test "$SESSION_R1V0" = "$SESSION_R2V0" || fail "Routers received different v0 sessions. See $PDU_DIR"
+ck_inc && test "$SESSION_R1V1" = "$SESSION_R2V1" || fail "Routers received different v1 sessions. See $PDU_DIR"
+ck_inc && test "$SESSION_R1V2" = "$SESSION_R2V2" || fail "Routers received different v2 sessions. See $PDU_DIR"
+
+ck_inc && test "$SESSION_R1V0" -ne "$SESSION_R1V1" || fail "RTRv0 session equals RTRv1 session. See $PDU_DIR"
+ck_inc && test "$SESSION_R1V1" -ne "$SESSION_R1V2" || fail "RTRv1 session equals RTRv2 session. See $PDU_DIR"
+ck_inc && test "$SESSION_R1V0" -ne "$SESSION_R1V2" || fail "RTRv0 session equals RTRv2 session. See $PDU_DIR"
