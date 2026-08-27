acd6df8683846e9ccec3c8edc7e62a6c2fa7471d
Alberto Leiva Popper <ydahhrk@gmail.com>
2026-08-27 11:46:21 -0600

    Skip rfc9286/6.1-1
    Again, I'm not sure if this is a bug in the spec or not.
    But it isn't urgent, so postponing until after Fort 2.

--- tests/rfc9286/6.1-1-files-not-co-residing-at-manifest-publication-point/run.sh
+++ tests/rfc9286/6.1-1-files-not-co-residing-at-manifest-publication-point/run.sh
@@ -3,6 +3,14 @@
 . tools/checks.sh
 . rp/$RP.sh
 
+# issues#178 and issues#181.
+# Fort behavior seems compliant, but the spec itself seems questionable.
+# WONTFIX for now. Will come back to this after Fort 2.0.
+# Related:
+# - rfc6481/2.2-c-object-published-outside-sia-location-ca 
+# - rfc9286/6.2-1-manifest-unreachable-via-sia-uri and
+test "$RP" = "fort2" && skip
+
 run_barry rd1
 run_rp
 
